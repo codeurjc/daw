2c822d74ad1f578f1210263852de1539a349ff52
micaelgallego <micael.gallego@gmail.com>
2021-03-14 23:21:16 +0100

    Remove unnecessary @DynamicUpdate from Book

--- sample-project-phase1/src/main/java/es/codeurjc/daw/library/model/Book.java
+++ sample-project-phase1/src/main/java/es/codeurjc/daw/library/model/Book.java
@@ -9,10 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 
-import org.hibernate.annotations.DynamicUpdate;
-
 @Entity
-@DynamicUpdate
 public class Book {
 
 	@Id
