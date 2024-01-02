18238d6db96b972c330fcdda7c568e7d93abad72
Maes95 <michel.maes95@gmail.com>
2024-01-02 21:51:19 +0100

    Update security_ejem6b

--- parte_2/tema_5/security_ejem6b/src/main/java/es/codeurjc/security/WebController.java
+++ parte_2/tema_5/security_ejem6b/src/main/java/es/codeurjc/security/WebController.java
@@ -1,11 +1,11 @@
 package es.codeurjc.security;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class WebController {
